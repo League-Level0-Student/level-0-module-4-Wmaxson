22478b10d21303a781e3c5cdf6f33d50e8f0f753
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2017-12-05 19:21:27 -0800

    Sleepy head

--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -16,13 +16,31 @@ public class SleepyHead {
          * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
          * JOptionPane.YES_NO_OPTION);
          */
-    
+        int answer = JOptionPane.showConfirmDialog(null, "is it a weekday?", "", JOptionPane.YES_NO_OPTION);
+        int answer1 = JOptionPane.showConfirmDialog(null, "R u on vacation??", "", JOptionPane.YES_NO_OPTION);
 
-
+        isWeekday = IntToBo(answer);
+        isVacation = IntToBo(answer1);
+        
         /*
          * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
          * print “get up lazybones!” If it is a weekday, and we are on vacation,
          * print “sleep in”.
          */
+        if (isVacation == true || isWeekday == false) {
+        	JOptionPane.showMessageDialog(null, "sleep in. Wait... if you you awnserd these queastions, why, are you asking a computer if you shold sleep in. Most poeple can make these connections their mind, maybe, you could go to a hospital and see a doctorr about this... ");
+			
+		}else {
+			JOptionPane.showMessageDialog(null, "Get up! *Incert sound effect of pots banging together*");
+			
+		}
     }
+   static boolean IntToBo(int y) {
+	   if (y == 0) {
+		   return true;
+		   
+   } else {
+	   return false;
+   }
+   }
 }
--- src/wordCount.java
+++ src/wordCount.java
@@ -2,6 +2,7 @@ import javax.swing.JOptionPane;
 
 public class wordCount {
 public static void main(String[] args) {
+	
 	String theWord = JOptionPane.showInputDialog("What word would you like to repeat");
 	String theNumberInWordForm = JOptionPane.showInputDialog("How many times would you like to repeat that word?");
 	int theNumber = Integer.parseInt(theNumberInWordForm);
